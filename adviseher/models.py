3c9fe08b2b27cd76582ab9fd748bbfd5c5f60831
morgan <morgan@1026labs.com>
2016-11-05 21:50:50 -0700

    added models to main site dir

--- adviseher/models.py
+++ adviseher/models.py
@@ -1,6 +1,16 @@
 from django.db import models
-from
 
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=25)
+    first = models.CharField(max_length=20)
+    last = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=2)
+    mentee_mentor_both = models.IntegerField(default=2)
+    intersts = models.CharField(max_length=50)
+class Profile:
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 class Question(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question_id = models.Autofield(primary_key=True)
@@ -11,4 +21,4 @@ class Answer(models.Model):
     question_id = ForeignKey(Question, on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=1000)
     upvotes = models.IntegerField(default=0)
-    
+
