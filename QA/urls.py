c141d484e332ea8b6bbb786e3cf21ebfffb7a1e4
morgan <morgan@1026labs.com>
2016-11-05 23:44:34 -0700

    fixed DB models

--- QA/urls.py
+++ QA/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^', views.index, name='index'),
-    url((r'^?P<question_id>[0-9]+)/$', views.question),
-
+    url(r'^(?P<question_id>[0-9]+)/$', views.question, name='question'),
 	]
--- QA/views.py
+++ QA/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import get_objects_or_404, render
-from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+#from django.http import Http404
 
-from adviseher.models import Question
-# TODO: how to import models from main level?
+from .models import Question
 
 def index(request):
     return HttpResponse("Hello, world. You're at the QA index.")
-def detail(request, question_id):
+def question(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'QA/detail.html', {'question': question})
 #TODO: include answers too
