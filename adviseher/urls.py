9e5a6e42a8ed055f1b97f97c32274b4fae439af7
Matt <hassan.m.sajwani@gmail.com>
2016-11-05 20:55:01 -0700

    Adding changes to settings and url.py.

--- adviseher/urls.py
+++ adviseher/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^matchmake', include('find_matches.urls'))
+    #url(r'^matchmake', include('find_matches.urls'))
 ]
