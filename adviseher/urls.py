97253dd73a736ac0528043177d45ce0cd1b103a5
Matt <hassan.m.sajwani@gmail.com>
2016-11-05 21:03:27 -0700

    modified urls.

--- adviseher/urls.py
+++ adviseher/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #url(r'^matchmake', include('find_matches.urls')),
-    url(r'^QA/', include.('QA.urls')),
+    url(r'^matchmake/', include('find_matches.urls')),
+    url(r'^QA/', include('QA.urls')),
 ]
